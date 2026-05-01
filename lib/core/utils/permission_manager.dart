6eb215d0d8541011ab98ee2b6b43702ce27813db
bhavya-durgani <d25cs112@charusat.edu.in>
2026-05-01 21:41:05 +0530

    Update permission_manager.dart

--- lib/core/utils/permission_manager.dart
+++ lib/core/utils/permission_manager.dart
@@ -1,19 +1,40 @@
-import 'package:permission_handler/permission_handler.dart';
+import 'package:permission_handler/permission_handler.dart';  
+// Imports permission_handler package (used to request device permissions)
 
-class PermissionManager {
-  static Future<bool> requestCameraPermission() async {
-    var status = await Permission.camera.status;
-    if (status.isDenied) {
-      status = await Permission.camera.request();
+class PermissionManager {  
+  // Class to handle permission requests (camera, storage, etc.)
+
+  static Future<bool> requestCameraPermission() async {  
+    // Function to request camera permission (returns true/false)
+
+    var status = await Permission.camera.status;  
+    // Check current camera permission status
+
+    if (status.isDenied) {  
+      // If permission is denied
+
+      status = await Permission.camera.request();  
+      // Ask user to allow camera permission
     }
-    return status.isGranted;
+
+    return status.isGranted;  
+    // Return true if permission is granted, otherwise false
   }
 
-  static Future<bool> requestStoragePermission() async {
-    var status = await Permission.storage.status;
-    if (status.isDenied) {
-      status = await Permission.storage.request();
+  static Future<bool> requestStoragePermission() async {  
+    // Function to request storage permission
+
+    var status = await Permission.storage.status;  
+    // Check current storage permission status
+
+    if (status.isDenied) {  
+      // If permission is denied
+
+      status = await Permission.storage.request();  
+      // Ask user to allow storage permission
     }
-    return status.isGranted;
+
+    return status.isGranted;  
+    // Return true if permission is granted
   }
 }
